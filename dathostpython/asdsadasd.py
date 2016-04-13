0c19e9cfcecf1161521a013fe0adb2cab8c9d7f8
Miles Budden <miles@budden.net>
2016-04-13 18:04:04 +0100

    Updated readme

--- dathostpython/asdsadasd.py
+++
@@ -1,5 +0,0 @@
-from wrapper import dathost
-
-dathost = dathost("miles@budden.net", "dathost12349876$^")
-
-print(dathost.edit("570e1dadcde5f567e658b6f7", csgo_settings__rcon = "Bob", name = "Bob"))
